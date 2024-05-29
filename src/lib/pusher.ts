1cf32b7e9753969afabfa0e9c9b78b01ec9d3a06
Rohit Chaware <80148306+R4Rohit23@users.noreply.github.com>
2024-05-29 10:22:33 +0530

    fix: exposed pusher

---
+++ src/lib/pusher.ts
@@ -0,0 +1,19 @@
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+
+export const pusherServer = new PusherServer({
+    appId: process.env.NEXT_PUSHER_APP_ID!,
+    key: process.env.NEXT_PUSHER_APP_KEY!,
+    secret: process.env.NEXT_PUSHER_SECRET!,
+    cluster: process.env.NEXT_PUSHER_CLUSTER!,
+    useTLS: true
+});
+
+export const pusherClient = new PusherClient(
+    process.env.NEXT_PUSHER_APP_ID!,
+    {
+        cluster: process.env.NEXT_PUSHER_CLUSTER!,
+    }
+)
+
