5db67b924fde328c6513656adae7d4ab2fa8f6d2
Rohit Chaware <80148306+R4Rohit23@users.noreply.github.com>
2024-06-03 18:11:27 +0530

    ads fix

--- src/_document.js
+++
@@ -1,7 +0,0 @@
-<Head>
-	<script
-		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4788424756296844"
-		crossorigin="anonymous"
-	></script>
-</Head>;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Navbar from "@/common/Navbar";
 import HomePage from "@/components/Home/Home";
 
+
 export default function Home() {
 	return (
 		<div className="bg-dark-1">
